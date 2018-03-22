0aacbda3f6e565aeb7717bb6ca68aa778cbe9f49
biancafloriana07 <biancafloriana07@gmail.com>
2018-03-22 17:58:17 +0200

    Reservation,Client,Person (domain model, mapper, gateway) -findbyid - comment Statement, ResultSet - separate closeConnection for findAll()

---
+++ HelloStudent/src/main/java/domainLayer/Mapper/ReservationMapper.java
@@ -0,0 +1,73 @@
+package domainLayer.Mapper;
+
+import database.ReservationGateway;
+import domainLayer.Reservation;
+import Exception.*;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReservationMapper {
+    private ReservationGateway reservationGateway ;
+
+    public ReservationMapper() {
+        this.reservationGateway = new ReservationGateway();
+    }
+
+
+    public void insert(Reservation reservation)
+    {
+        try{
+            reservationGateway.insert(reservation.getClient(),reservation.getDestination(),reservation.getHotel(),reservation.getPersonNumber(),
+                    reservation.getPrice(),reservation.getDate(),reservation.getPartialPayment(),reservation.isPaid());
+        }catch(ReservationGatewayException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void update(Reservation reservation)
+    {
+        try{
+            reservationGateway.update(reservation.getIdReservation(),reservation.getClient(),reservation.getDestination(),reservation.getHotel(),reservation.getPersonNumber(),
+                    reservation.getPrice(),reservation.getDate(),reservation.getPartialPayment(),reservation.isPaid());
+        }catch(ReservationGatewayException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void delete(Reservation reservation)
+    {
+        try{
+            reservationGateway.delete(reservation.getIdReservation());
+        }catch(ReservationGatewayException e){
+            e.printStackTrace();
+        }
+    }
+
+    public List<Reservation> findAll()
+    {
+        try{
+            ResultSet r = reservationGateway.findAll();
+            List<Reservation> reservationList = new ArrayList<>();
+
+            while(r.next()){
+
+
+                reservationList.add( new Reservation(r.getInt("idReservation"),r.getInt("idClient"),r.getString("destination"),
+                        r.getString("hotel"),r.getInt("personNumber"),r.getInt("price"),r.getDate("date"),
+                        r.getInt("partialPayment"),r.getBoolean("paid")));
+
+
+            }
+            reservationGateway.closeConnection();
+            return reservationList;
+        }catch(ReservationGatewayException | SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+}
---
+++ HelloStudent/src/main/java/domainLayer/Reservation.java
@@ -0,0 +1,114 @@
+package domainLayer;
+
+import java.util.Date;
+
+public class Reservation {
+
+    private int idReservation;
+    private int client;
+    private String destination;
+    private String hotel;
+    private int personNumber;
+    private int price;
+    private Date date;
+    private int partialPayment;
+    private boolean paid;
+
+    public Reservation(int client, String destination, String hotel, int personNumber, int price, Date date, int partialPayment, boolean paid) {
+        this.client = client;
+        this.destination = destination;
+        this.hotel = hotel;
+        this.personNumber = personNumber;
+        this.price = price;
+        this.date = date;
+        this.partialPayment = partialPayment;
+        this.paid = paid;
+    }
+
+    public Reservation(int idReservation, int client, String destination, String hotel, int personNumber, int price, Date date, int partialPayment, boolean paid) {
+        this.idReservation = idReservation;
+        this.client = client;
+        this.destination = destination;
+        this.hotel = hotel;
+        this.personNumber = personNumber;
+        this.price = price;
+        this.date = date;
+        this.partialPayment = partialPayment;
+        this.paid = paid;
+    }
+
+    public int getIdReservation() {
+        return idReservation;
+    }
+
+    public void setIdReservation(int idReservation) {
+        this.idReservation = idReservation;
+    }
+
+    public int getClient() {
+        return client;
+    }
+
+    public void setClient(int client) {
+        this.client = client;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(String hotel) {
+        this.hotel = hotel;
+    }
+
+    public int getPersonNumber() {
+        return personNumber;
+    }
+
+    public void setPersonNumber(int personNumber) {
+        this.personNumber = personNumber;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getPartialPayment() {
+        return partialPayment;
+    }
+
+    public void setPartialPayment(int partialPayment) {
+        this.partialPayment = partialPayment;
+    }
+
+    public boolean isPaid() {
+        return paid;
+    }
+
+    public void setPaid(boolean paid) {
+        this.paid = paid;
+    }
+
+
+
+}
